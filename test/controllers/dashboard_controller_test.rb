fd69190a2b43841e843f232d2be780b68f9ff814
Sofhack009 <sofhack579@gmail.com>
2026-08-02 16:29:26 +0530

    Fix dashboard route test

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboard_index_url
-    assert_response :success
+  test "redirects unauthenticated users" do
+    get root_url
+
+    assert_redirected_to new_user_session_url
   end
 end
