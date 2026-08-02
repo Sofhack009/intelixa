1f243b6d684628b5f8521e33df55923725b30afb
Sofhack009 <sofhack579@gmail.com>
2026-08-02 16:30:13 +0530

    Connect warehouses to stock movement ledger

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,7 @@
 class Warehouse < ApplicationRecord
   has_many :inventory_batches, dependent: :restrict_with_error
+  has_many :stock_movements, dependent: :restrict_with_error
+  has_many :wastage_logs, dependent: :restrict_with_error
 
   validates :name, presence: true, uniqueness: true
 end
